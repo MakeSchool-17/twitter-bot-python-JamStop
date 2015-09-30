2120edfca4f858fa57f2aa23025a244cfe7df392
Emil Shirima <emil.shirima@hotmail.com>
2015-09-30 16:07:07 -0700

    Start tokenizing

--- tokenize.py
+++ tokenize.py
@@ -1,1 +1,15 @@
 ''' Time to token marx! '''
+
+
+import re
+
+
+class Tokenizer:
+    def __init__(self, text):
+        self.data = text
+
+    def _marx(self):
+        pass
+
+if __name__ == "__main__":
+    t = Tokenizer('hahahaha')
