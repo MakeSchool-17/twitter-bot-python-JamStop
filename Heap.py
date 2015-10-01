823a7053efbb2efba50f70ecfe3a2bd6d9c33a8a
JamStop <jimmyyue1@gmail.com>
2015-09-30 22:06:36 -0700

    Heap update

--- Heap.py
+++ Heap.py
@@ -6,9 +6,30 @@ class Heap:
         self.data = [None]
         self.size = 0
 
-    def percolate(self, size):
-        while size // 2 > 0:
-            if self.data[size] > self.data[size // 2]:
-                value = self.data[size // 2]
-                self.data[size // 2] = self.data[size]
-                self.data[size] = value
+    ''' Percolate up in order to move an appended index to the right place '''
+    def percolate_up(self, index):
+        while index // 2 > 0:                               # work way up to root
+            if self.data[index] < self.data[index // 2]:    # check if we still need to move
+                value = self.data[index // 2]               # keep track of what we're moving
+                self.data[index // 2] = self.data[index]    # move our item up
+                self.data[index] = value                    # switch the other one in
+            index = index // 2                              # we move up the heap
+
+    ''' Percolate down in order to reset a heap to proper structure '''
+    def percolate_down(self, index):
+        while index * 2 <= self.self.size:
+
+
+    def insert(self, value):
+        self.data.append(value)
+        self.size += 1
+        self.percolate_up
+
+    def get_min_child(self, index):                                 #
+        if index * 2 + 1 > self.size:
+            return index * 2
+        else:
+            if self.data[index * 2] < self.data[index * 2 + 1]:
+                return index * 2
+            else:
+                return index * 2 + 1
--- tokenize.py
+++ tokenize.py
@@ -9,11 +9,12 @@ class Tokenizer:
         self.data = text
 
     def _marx(self):
+        # Todo: fixme
+        marx_split = '[] '
         marxer = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
         for line in self.data:
             for word in punct.split(str(line)):
-
-
+                pass
 
 if __name__ == "__main__":
     t = Tokenizer('hahahaha')
